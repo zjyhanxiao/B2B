8c1358a3ea9fc826440c687ea8d04d9df403ed66
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2017-02-20 17:57:27 +0800

    辅助下单

--- local/version/js/common.js
+++ local/version/js/common.js
@@ -5,7 +5,11 @@ var is_admin;
 //获取url中的参数
 function getUrlParam(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-    var r = decodeURI(window.location.search).substr(1).match(reg);  //匹配目标参数
+    var s = window.location.search;
+    if (/[\u4e00-\u9fa5]/.test(s)) {
+        s=decodeURI(s);
+    }
+    var r = s.substr(1).match(reg);  //匹配目标参数
     if (r != null) return unescape(r[2]);
     return null; //返回参数值
 }
