bc1acf0945dc4122147e833c40c32e19d1a27c6f
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2017-02-21 16:16:19 +0800

    辅助下单

--- local/version/js/auxiliary_order.js
+++ local/version/js/auxiliary_order.js
@@ -3,6 +3,7 @@ $(function () {
     var jSignatureData = $('#signature-line').jSignature('getData');
     var order_number = getUrlParam('order_number') || '';
     var product_id = getUrlParam('product_id') || '';
+    var pdf = null;
     getData({
         url: base_url + '/white_label/product_info',
         data: {product_id: product_id},
@@ -28,20 +29,50 @@ $(function () {
 
     });
     // 点击'点击查看投资信息'查看投资信息
-    $('.show-user-info a').on('click',function () {
-       $(this).hide();
-       $('.user-info').show();
+    $('.show-user-info a').on('click', function () {
+        $(this).hide();
+        $('.user-info').show();
     });
-    $('.hide-info').on('click',function () {
+    $('.hide-info').on('click', function () {
         $('.user-info').hide();
         $('.show-user-info a').show();
     });
+
+    // 提交签名信息
+    $('#update_order').on('click', function () {
+        $('#signature-box').css('border', '1px solid #ccc');
+        $('.document-unCheck').remove();
+        $(this).prop('disabled', true);
+        var signature = $('#signature-line').jSignature('getData');
+        var checkedLen = $('.document-item input:checked').length;
+        var checkBoxLen = $('.document-item input[type="checkbox"]').length;
+        if (checkBoxLen != checkedLen) {
+            $('.document-item').append('<div style="color:red" class="document-unCheck">请认真阅读并勾选以上所有文件</div>');
+            return false;
+        }
+        if (signature == jSignatureData) {
+            $('#signature-box').css('border', '1px solid red');
+            $('body').scrollTop($('#signature-box').offset().top);
+            return false;
+        }
+        postData({
+            url: base_url + '/zion/assist/updateSignature',
+            data: JSON.stringify({order_number: order_number, signature: signature}),
+            contentType: "application/json; charset=utf-8",
+            sucFn: updateSignature,
+            failFn: failFn
+        });
+        return false;
+    });
+    function updateSignature(res) {
+        window.location = '/auxiliary_order/invest_success.html?order_number=' + order_number
+    }
+
     // 输入解锁密码打开签名页面
     function getUserData(res) {
         var d = res.body;
         if (d && d != null) {
             $('.beforeOpen').hide();
-            $('#signaturePage').show();
             $('.product_name').html(d.product_name);
             $('.return_rate').html('预计年化收益：' + d.return_rate + '%');
             $('.invest_term').html('投资期限：' + d.invest_term + '个月');
@@ -80,18 +111,72 @@ $(function () {
                     $('.middle_bank_address').html(ban_non_us.middle_bank_address);
                     $('.middle_bank_swift_code').html(ban_non_us.middle_bank_swift_code);
                 }
+                $('#signaturePage').show();
             }
-            var document_list='';
-            $.each(d.product_document,function (index,item) {
-                if(item.need_mapped){
-
+            var document_list = '';
+            pdf = d.order_user_info;
+            $.each(d.product_document, function (index, item) {
+                if (item.need_mapped) {
+                    document_list += '<div class="checkbox">' +
+                        '<label>' +
+                        '<input type="checkbox">我已阅读并接受' +
+                        '</label>' +
+                        '<a data-id="' + item.id + '" class="getPdf">' + item.document_name + '</a>' +
+                        '</div>'
+                } else {
+                    document_list += '<div class="checkbox">' +
+                        '<label>' +
+                        '<input type="checkbox">我已阅读并接受' +
+                        '</label>' +
+                        '<a href="' + item.document_url + '">' + item.document_name + '</a>' +
+                        '</div>'
                 }
             })
-
-
+            $('.document-item').html(document_list);
         }
     }
 
+    /* Open document preview */
+    function openDocumentPreview(document_id, populate_data) {
+        $("#document-preview").width("100%");
+
+        $(function () {
+            $('#document-loading').show();
+            $.ajax({
+                type: "post",
+                url: base_url + '/channel/doc/preview',
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(populate_data),
+                success: populate
+            });
+
+            function populate(res) {
+                $('#document-loading').hide();
+                $("#document-element-wrapper").html('<iframe id="document-preview-element" src="/vendor/pdfjs/web/viewer.html?file=' + res.body + '#page=1"></iframe>');
+                $("#document-preview-element").width($(window).width());
+                $("#document-preview-element").height($(window).height() - 45);
+                $("#document-preview-element").css('margin-top', '40px');
+            }
+        })
+    }
+
+    /* Close when someone clicks on the "x" symbol inside the overlay */
+    function closeDocumentPreview() {
+        $("#document-element-wrapper").html("");
+        $("#document-preview").width("0%");
+    }
+
+    $(".document-item").on('click', '.getPdf', function () {
+        var document_id = $(this).data('id');
+        pdf.document_id = document_id;
+        //映射文档
+        openDocumentPreview(document_id, pdf);
+        return false;
+    });
+
+    $("#document-preview .close-document-preview").click(function () {
+        closeDocumentPreview();
+    });
     function getProductSuc(res) {
         var d = res.body;
         if (d) {
