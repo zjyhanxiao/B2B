8ce4e3db2a8cd8fb03bc4d90c5658e8fe4cbd299
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2017-03-03 15:14:51 +0800

    辅助下单

--- local/version/js/voucher.js
+++ local/version/js/voucher.js
@@ -7,7 +7,7 @@ $(function () {
     $('#get_code').on('click', function () {
         phone = $("#phone").val();
         var $that = $(this);
-        if(phone==''){
+        if(!/[1]d{10}$/.test(phone)){
             $("#error").html('请输入您的手机号码');
             return false;
         }else{
@@ -38,7 +38,7 @@ $(function () {
     $('#submit').on('click', function () {
         phone = $("#phone").val();
         var verify_code = $("#verify_code").val();
-        if(phone==''){
+        if(!/^1\d{10}$/.test(phone)){
             $("#error").html('请输入您的手机号码');
             return false;
         }if(verify_code==''){
@@ -77,15 +77,3 @@ $(function () {
         $("#error").css('visibility','hidden');
     })
 });
-
-//获取url中的参数
-function getUrlParam(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-    var s = window.location.search;
-    if (/[\u4e00-\u9fa5]/.test(s)) {
-        s = decodeURI(s);
-    }
-    var r = s.substr(1).match(reg);  //匹配目标参数
-    if (r != null) return unescape(r[2]);
-    return null; //返回参数值
-}
