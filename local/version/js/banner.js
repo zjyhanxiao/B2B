544bf63754ad4bb405bf2e1f38230911152dfb5f
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2017-03-08 18:00:24 +0800

    辅助下单

--- local/version/js/banner.js
+++ local/version/js/banner.js
@@ -12,55 +12,55 @@ $(function () {
         controller: $('#bannerCtrl'),
         activeControllerCls: 'active'
     });
-    $('#banner_tabs .flex-prev').click(function() {
+    $('#banner_tabs .flex-prev').click(function () {
         bannerSlider.prev()
     });
-    $('#banner_tabs .flex-next').click(function() {
+    $('#banner_tabs .flex-next').click(function () {
         bannerSlider.next()
     });
 
 
     $('.phone').mouseover(function () {
-        $('.phone_before').css('display','none')
-        $('.phone_after').css('display','block')
+        $('.phone_before').css('display', 'none')
+        $('.phone_after').css('display', 'block')
     });
     $('.phone').mouseout(function () {
-        $('.phone_before').css('display','block')
-        $('.phone_after').css('display','none')
+        $('.phone_before').css('display', 'block')
+        $('.phone_after').css('display', 'none')
     });
 
     var d, h;
-/*    function scroll( fn ) {
-        var beforeScrollTop = $(window).scrollTop(),
-            fn = fn || function() {};
-        window.addEventListener("scroll", function() {
-            var afterScrollTop = $(window).scrollTop(),
-                delta = afterScrollTop - beforeScrollTop;
-            if( delta === 0 ) return false;
-            fn( delta > 0 ? "down" : "up" );
-            beforeScrollTop = afterScrollTop;
-        }, false);
-    }
-    scroll(function(direction) {
-        d =  direction;
-        h = $(document).scrollTop();
-        var $header = $(".nav-bg");
-        var $headerWhite = $(".nav-bg-white");
-        if(d=="up"){
-       // console.log(d);
-       // console.log(h);
-            if(h<600){
-                $header.fadeIn();
-                $headerWhite.fadeOut();
-            }else if(h>600){
-                $header.fadeOut();
-                $headerWhite.fadeIn();
-            }
-        }else{
-            $header.hide();
-            $headerWhite.hide();
-        }
-    });*/
+    /*    function scroll( fn ) {
+     var beforeScrollTop = $(window).scrollTop(),
+     fn = fn || function() {};
+     window.addEventListener("scroll", function() {
+     var afterScrollTop = $(window).scrollTop(),
+     delta = afterScrollTop - beforeScrollTop;
+     if( delta === 0 ) return false;
+     fn( delta > 0 ? "down" : "up" );
+     beforeScrollTop = afterScrollTop;
+     }, false);
+     }
+     scroll(function(direction) {
+     d =  direction;
+     h = $(document).scrollTop();
+     var $header = $(".nav-bg");
+     var $headerWhite = $(".nav-bg-white");
+     if(d=="up"){
+     // console.log(d);
+     // console.log(h);
+     if(h<600){
+     $header.fadeIn();
+     $headerWhite.fadeOut();
+     }else if(h>600){
+     $header.fadeOut();
+     $headerWhite.fadeIn();
+     }
+     }else{
+     $header.hide();
+     $headerWhite.hide();
+     }
+     });*/
 
 
     var scrollFunc = function (e) {
@@ -68,32 +68,17 @@ $(function () {
         var $headerWhite = $(".nav-bg-white");
         h = $(document).scrollTop();
         e = e || window.event;
-        if (e.wheelDelta>0) {
-            console.log(e.wheelDelta);
+        if (e.wheelDelta > 0 || e.detail < 0) {
             //如果是IE/Opera/Chrome浏览器
-            if(h<100){
+            if (h < 100) {
                 $header.fadeIn();
-                $headerWhite.fadeOut();
-            }if(h>=100){
-                console.log(h);
-                $header.fadeOut();
+                $headerWhite.hide();
+            }
+            if (h >= 100) {
+                $header.hide();
                 $headerWhite.fadeIn();
             }
-        }else {
-            $header.hide();
-            $headerWhite.hide();
-        }
-
-        if (e.detail>0) {
-            //如果是Firefox浏览器
-            if(h<100){
-                $header.fadeIn();
-                $headerWhite.fadeOut();
-            }if(h>=100){
-                $header.fadeOut();
-                $headerWhite.fadeIn();
-            }
-        }else {
+        } else {
             $header.hide();
             $headerWhite.hide();
         }
@@ -102,7 +87,6 @@ $(function () {
     if (window.addEventListener) {
         //非firefox
         window.addEventListener('mousewheel', scrollFunc, false);
-
         //firefox
         window.addEventListener('DOMMouseScroll', scrollFunc, false);
     }
