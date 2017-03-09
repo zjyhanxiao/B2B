f660c10fce57bb9b1510df228fcdcba95666d6f2
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2017-03-09 18:19:25 +0800

    修改辅助下单

--- local/version/js/invest_flow.js
+++ local/version/js/invest_flow.js
@@ -1,4 +1,5 @@
 $(function () {
+    alert(1111);
     var product_id = getUrlParam('product_id') || '';
     var order_number = getUrlParam('order_number') || '';
     $('#fileMapping').height(Math.floor($('#fileMapping').width() * 200 / 340));  //设置图片上传组件高度
